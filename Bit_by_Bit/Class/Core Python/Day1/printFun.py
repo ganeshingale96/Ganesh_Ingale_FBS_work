3313d779e2fb99b1d01ab9c01c64714846c59efa
Ganesh Ingale <ganeshingale964@gmail.com>
2025-07-28 23:22:35 +0530

    Add files via upload

---
+++ Bit_by_Bit/Class/Core Python/Day1/printFun.py
@@ -0,0 +1,5 @@
+var= 5
+print(var)
+print(f"Variable:-{var}")
+print("Variable:-",var)
+print("Variable:-",format (var))
---
+++ Bit_by_Bit/Class/Core Python/Day2/operator.py
@@ -0,0 +1,36 @@
+#<<<<<<<<<<<<<<<<<<<<<<<Operator>>>>>>>>>>>>>>>>>>>>>
+a = 10
+b = 20
+c = 'abc'
+d = 'efg'
+
+# Addition (+)
+print(a+b)
+print(c+d)
+# print(a+d) TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# Subtraction (-)
+print(a-b)
+# print(c-d) # TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+# Multiply
+print(a*b)
+
+# Divide
+print(a/b)
+
+# Floor division
+print(a//b)
+
+# Remainder
+print(a%b)
+
+# Exponentiation
+print(a**b)
+
+
+
+
+
+
+
