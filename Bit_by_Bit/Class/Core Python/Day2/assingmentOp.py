5b7454ecc233139dba67e7fe81b91acd05111c68
Ganesh Ingale <ganeshingale964@gmail.com>
2025-07-28 23:18:44 +0530

    Add files via upload

---
+++ Bit_by_Bit/Class/Core Python/Day2/assingmentOp.py
@@ -0,0 +1,11 @@
+
+# Assignment Operators Example
+a1 = 5      # =
+a1 += 10    # +=
+a1 -= 100   # -=
+a1 /= 100   # /=
+a1 *= 100   # *=
+a1 %= 100   # %=
+a1 **= 10   # **=
+
+print(a1)
---
+++ Bit_by_Bit/Class/Core Python/Day2/comparisionOp.py
@@ -0,0 +1,27 @@
+a = 10
+b = 20
+
+# Equal to
+print(a == b)
+
+# Not equal to
+print(a != b)
+
+# Greater than
+print(a > b)
+
+# Less than
+print(a < b)
+
+# Greater than or equal to
+print(a >= b)
+
+# Less than or equal to
+print(a <= b)
+#<<<<<<<<<<<<<<<<<<<<<<<Output>>>>>>>>>>>>>>>>>>>>>
+# False
+# True
+# False
+# True
+# False
+# True
